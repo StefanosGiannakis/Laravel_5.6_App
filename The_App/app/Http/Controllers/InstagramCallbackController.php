6cab90c218e89a92105a692b3e2716ec98cd3779
satchfast <satchfast@gmail.com>
2018-05-11 12:13:19 +0300

    Login done

--- The_App/app/Http/Controllers/InstagramCallbackController.php
+++ The_App/app/Http/Controllers/InstagramCallbackController.php
@@ -19,12 +19,14 @@ class InstagramCallbackController extends Controller
     public function __construct(In $InstagramController)
     {
         $this->Obj = new $InstagramController;
-    }   
-
-    // public function __construct($InstagramObj)
-    // {
-    //    $this->Instagram = $InstagramObj;
-    // }
+    }
+    public function ifAlreadyAuser($user)// bcz laravel is already checking if simple user exists 
+    {
+        Auth::login($user);
+        return redirect()->route('home');
+        // return $user;
+    }
+   
     public function callback()
     {
         $this->data = $this->Obj->getAccesTokenAndUserDetails($_GET['code']);
@@ -40,56 +42,53 @@ class InstagramCallbackController extends Controller
         $_SESSION['fullname'] = $this->data->user->full_name;
         $_SESSION['profilePicture'] = $this->data->user->profile_picture;
 
+
         //We need a function to check if an instagram user exists
-        
+        $EMAIL = $this->data->user->username . '@' . 'myapp' . '.com';
+        $user = User::where('email', $EMAIL)->first();
+        if (!$user) {
+            $this->createUser();
+        }
         // auth()->login($user);
-        return $this->createUser();
+        return $this->ifAlreadyAuser($user);
 
     }
 
     public function createUser()
     {
-        $EMAIL=$this->data->user->username.'@'.'myapp'.'.com' ;
-        $Obj= new User;
+        $EMAIL = $this->data->user->username . '@' . 'myapp' . '.com';
+        $Obj = new User;
        //dd( $request->all());
           //Create a user for every Instagram User registered
-          $Obj::create([
+        $Obj::create([
             'name' => $this->data->user->username,
-            'email' =>$EMAIL,
+            'email' => $EMAIL,
             'password' => null
         ]);
-           
+
 
         $userDB = User::where('email', $EMAIL)->first();
-        // Log in the user 
-        Auth::login($userDB);
-        // $todos=User::where('email',$EMAIL)->get('id');    
-        // $userDB = DB::table('users')
-        //         ->whereColumn([
-        //             ['email', '=', $EMAIL],
-        //         ])->get();
-        // dd($userDB->id);
-        // dd(Auth::check());
+     
         return $this->CreateInstagramUser($userDB->id);
 
-    }                                        
+    }
 
     public function CreateInstagramUser($id)
     {
         // find user 
-        $user = User::where('email',$this->data->user->username.'@'.'myapp'.'.com');
+        $user = User::where('email', $this->data->user->username . '@' . 'myapp' . '.com');
         // $rr=Auth::guard('web')->User();
         // dd($rr);
         // Create Instagram User
-        $user= new Instagram;
-        $user->instagram_id=$this->data->user->id;
-        $user->username= $this->data->user->username;
-        $user->userId= $id; ### Stef 
-        $user->fullname=$this->data->user->full_name;
-        $user->website=$this->data->user->website;
-        $user->bio=$this->data->user->bio;
-        $user->profilePicture= $this->data->user->profile_picture;
-        $user->provider='Instagram'; //assign the provider by hand
+        $user = new Instagram;
+        $user->instagram_id = $this->data->user->id;
+        $user->username = $this->data->user->username;
+        $user->userId = $id; ### Stef 
+        $user->fullname = $this->data->user->full_name;
+        $user->website = $this->data->user->website;
+        $user->bio = $this->data->user->bio;
+        $user->profilePicture = $this->data->user->profile_picture;
+        $user->provider = 'Instagram'; //assign the provider by hand
         $user->save();
         
         // Session::flash('success','Your user Created');
@@ -100,7 +99,7 @@ class InstagramCallbackController extends Controller
 
     public function loggedPage()
     {
-        return view('logged'); 
-    }                           
+        return view('logged');
+    }
 
 }
